ea57dda292a596b406ed45125fe801a1a2471440
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-01-16 11:16:01 +0000

    tema hijo con  2 sidebar perswonalizado

---
+++ themes/twentynineteen-child/functions.php
@@ -0,0 +1,41 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function twentynineteenchild_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Custom Footer bar',
+		'id'            => 'footer_bar',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+		register_sidebar( array(
+		'name'          => 'Custom Header bar',
+		'id'            => 'header_bar',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'twentynineteenchild_widgets_init' );
+?>
